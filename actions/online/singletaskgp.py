0c20c5385a3a58659179207ea376fad1fb803d2d
muonsontoast <shaun.t.preston@gmail.com>
2025-09-16 11:45:46 +0100

    Online GP action checks for valid inputs.

--- actions/online/singletaskgp.py
+++ actions/online/singletaskgp.py
@@ -1,6 +1,7 @@
 import numpy as np
 from multiprocessing.shared_memory import SharedMemory
 from ..action import Action
+from ...utils import cothread
 from ... import shared
 
 class SingleTaskGPAction(Action):
@@ -9,6 +10,7 @@ class SingleTaskGPAction(Action):
         super().__init__(parent)
 
     def CheckForValidInputs(self):
+        print('Online GP action is checking for valid inputs!')
         if len(self.parent.decisions) == 0:
             shared.workspace.assistant.PushMessage('Single Task GP is missing decision variables.', 'Error')
             return False
@@ -16,3 +18,12 @@ class SingleTaskGPAction(Action):
             shared.workspace.assistant.PushMessage('Single Task GP is missing objectives.', 'Error')
             return False
         # Attempt a caget on the decision variables to see if they are valid
+        try:
+            for d in self.decisions:
+                cothread.caget(d.name)
+            print('All online GP inputs exist on the machine!')
+        except Exception as e:
+            shared.workspace.assistant.PushMessage(f'{e}.', 'Error')
+            return False
+        
+        return True
