face076634f77209b74f0edbf27d28ca83929b02
muonsontoast <shaun.t.preston@gmail.com>
2026-02-18 01:51:31 +0000

    .

--- utils/multiprocessing.py
+++ utils/multiprocessing.py
@@ -123,5 +123,6 @@ def CreatePersistentWorkerThread(entity, inQueue, outQueue, action, **kwargs):
     except Exception as e:
         print(e)
         entity.updateAssistantSignal.emit(f'{entity.name} was unable to stop the LINAC. User should manually disable it now.', 'Warning')
+    print('POPPING >...... . ..  .. . ')
     runningActions.pop(entity.ID)
     # loop.close()
